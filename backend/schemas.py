d437e6a896e02ce8e535faf27e6af88b55d30a18
Turtle-sg <gnoths@etsu.edu>
2025-11-14 21:44:52 -0500

    Created the Pydantic schemas for all the db elements.

---
+++ backend/schemas.py
@@ -0,0 +1,50 @@
+'Schemas for all database tables to convert them to Json'
+from pydantic import BaseModel
+
+'The model for a course'
+class Course(BaseModel):
+    course_id:str
+    prefix:str
+    course:int
+    credits:int
+    title:str
+    description:str
+    prereq_notes:str
+    coreq_notes:str
+    prereq_id:str
+    coreq_id:str
+    degree_id:int
+
+    class Config:
+        orm_mode=True
+
+'The model for a course requisite'
+class CourseReq(BaseModel):
+    course_id:str
+    prereq_id:str
+    coreq_id:str
+
+    class Config:
+        orm_mode=True
+
+'The model for a degree'
+class Degree(BaseModel):
+    degree_id:int
+    title:str
+    level:str
+    type:str
+    concentration:str
+    department:str
+    description:str
+    course_id:str
+
+    class Config:
+        orm_mode=True
+
+'The model for a degree course'
+class DegreeCourse(BaseModel):
+    degree_id:int
+    course_id:str
+
+    class Config:
+        orm_mode=True
