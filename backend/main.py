06fed4e45dd42cfbd64be20f30c568451f3530a8
NoahFordGit <noahmichaelford@gmail.com>
2025-11-29 21:46:57 -0500

    finalized API entry point to include FastAPI routers

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,8 @@
 from fastapi import FastAPI
+from api.course_endpoints import router as course_router
+from api.degree_endpoints import router as degree_router
 
 app = FastAPI()
+
+app.include_router(course_router)
+app.include_router(degree_router)
